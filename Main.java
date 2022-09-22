5ebacbbd40484dab086da60ac93fc23cd6bf98d6
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-09-22 15:51:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+  System.out.println("Please enter your first and last name");
+  String input = input.nextLine();
+  
+   
+  }
+}
