b228dbfeb70ecc575a0f1648f80289666cbd5862
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-09-22 16:00:01 +0000

    Comments

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
+/**
+
+Jack Schumacher
+AP CS A
+Orange (5)
+Chapter 2- String Methods
+Programming Assignment 2
+September 2022
+
+*/
+
+
 import java.util.Scanner;
-
-
 class Main {
   public static void main(String[] args) {
   Scanner input = new Scanner(System.in);
-  System.out.println("Please enter your name");
+  System.out.println("Please enter your first name");
   String firstName = input.nextLine();
-  System.out.println("Ok!, your name is:"+ firstName);
+  System.out.println("Ok!, your first name is:"+ firstName);
+  System.out.println("Please enter your last name");
+  String lastName = input.nextLine();
+  System.out.println("Ok!, your last name is:")
+  
   
   
    
