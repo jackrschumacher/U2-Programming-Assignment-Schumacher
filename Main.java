b5a034279ec6f2de0adb9d394329848f381d6466
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-09-22 15:55:35 +0000

    First name integration and bug fixes

--- Main.java
+++ Main.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
   Scanner input = new Scanner(System.in);
-  System.out.println("Please enter your first and last name");
-  String input = input.nextLine();
+  System.out.println("Please enter your name");
+  String firstName = input.nextLine();
+  System.out.println("Ok!, your name is:"+ firstName);
+  
   
    
   }
