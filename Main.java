ba70539a43fba6ecdd1db2b8c89d00fe016e7031
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-09-22 16:09:41 +0000

    Substring Integration

--- Main.java
+++ Main.java
@@ -13,7 +13,8 @@ This program uses string methods in order to Create MASD emails.
 
 import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
+  // Have the user input thier first and last name - assign String variables for both the first and last name of the users
+  public static void main(String[] args) { 
   Scanner input = new Scanner(System.in);
   System.out.println("Please enter your first name");
   String firstName = input.nextLine();
@@ -21,7 +22,15 @@ class Main {
   System.out.println("Please enter your last name");
   String lastName = input.nextLine();
   System.out.println("Ok!, your last name is:")
-  
+
+  if(firstName.length() < 4){
+    firstName = firstName.substring(0);
+  else{
+    firstName = firstName.substring(0,4);
+  }
+    
+    
+  }
   
   
    
